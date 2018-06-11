c9bba7a5724b060b0ba368b16967498b44faca3d
kpilcicki <36018832+kpilcicki@users.noreply.github.com>
2018-06-11 04:50:39 +0200

    Finish up the last commit

--- IadZad3.Model/Utility/RadialNeuronPositioner/KMeansNeuronPositioner.cs
+++ IadZad3.Model/Utility/RadialNeuronPositioner/KMeansNeuronPositioner.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IAD_zad2.Model;
+using IAD_zad2.Utilities.Generators;
+using IadZad3.Model.Utility.TrainingParameter;
 
 namespace IadZad3.Model.Utility.RadialNeuronPositioner
 {
-    class KMeansNeuronPositioner
+    class KMeansNeuronPositioner : INeuronPositioner
     {
+        public void PositionNeurons(TrainingParameters trainingParameters, List<RadialNeuron> radialNeurons)
+        {
+            var SOMTrainingParameters = new IAD_zad2.Model.Parameters.KMeansTrainingParameters()
+            {
+                Epochs = 
+            };
+            var inputPoints = trainingParameters.InputPoints;
+            double minPosition = inputPoints.Min(elem => elem.Input.Min());
+            double maxPosition = inputPoints.Max(elem => elem.Input.Max());
+            int dimensions = inputPoints.First().Input.Count;
+
+
+            KMeansNetwork mapNetwork = new KMeansNetwork(radialNeurons.Count, new NeuronRandomRectangularInitializer(minPosition, maxPosition, dimensions), new IAD_zad2.Utilities.Distance.EuclideanDistance());
+            mapNetwork.Train(SOMTrainingParameters);
+            throw new NotImplementedException();
+        }
     }
 }
